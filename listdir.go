89a54d56305b74b34397aa153a9d50c21a3b19df
Willie Abrams <willie@pobox.com>
2018-10-10 08:49:23 -0500

    part of the way to finding partial dupes

--- listdir.go
+++ listdir.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"github.com/nyxnaut/fastwalk"
 )
+
+func compare(paths []string) {
+	m, err := os.Open(paths[0])
+	if err != nil {
+		return
+	}
+
+	offset := 0
+	mr := bufio.NewReader(m)
+
+	var o []*bufio.Reader
+	for _, n := range paths[1:] {
+		f, err := os.Open(n)
+		if err != nil {
+			return
+		}
+
+		o = append(o, bufio.NewReader(f))
+	}
+
+	c, err := mr.ReadByte()
+	if err != nil {
+		return
+	}
+
+	for _, t := range o {
+		offset = offset + 1
+		cp, err := t.ReadByte()
+		if err != nil {
+			return
+		}
+
+		if cp != c {
+			fmt.Println("differs at:", offset)
+
+		}
+	}
+	fmt.Println("finished compare at offset:", offset)
+
+}
 
 func main() {
 	var dirs []string
@@ -17,24 +55,44 @@ func main() {
 		dirs = append(dirs, ".")
 	}
 
+	f := make(map[int64][]string)
+
 	for _, root := range dirs {
-		err := fastwalk.Walk(root, func(path string, typ os.FileMode) error {
-			if typ.IsDir() {
+		filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			if info.IsDir() {
 				return nil
 			}
 
 			relative, _ := filepath.Rel(root, path)
-			if strings.Index(relative, ".") == 0 {
-				return nil
-			}
+			f[info.Size()] = append(f[info.Size()], relative)
 
 			fmt.Println(relative)
-			// paths = append(paths, relative)
+
 			return nil
 		})
 
-		if err != nil {
-			fmt.Println("fastwalk.Walk", err, root)
+		fmt.Println()
+
+		for k, v := range f {
+			if len(v) > 1 {
+				fmt.Println(k)
+
+				var paths []string
+				for _, n := range v {
+					fmt.Println(n)
+					paths = append(paths, n)
+				}
+
+				fmt.Println()
+
+				compare(paths)
+
+				fmt.Println()
+			}
 		}
 	}
 }
